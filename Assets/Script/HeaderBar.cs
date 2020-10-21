f4605ce0a2180b3e539aa3d6d7ac5669291c1cbd
Audrey Zhang <audreyymz@gmail.com>
2020-10-21 10:56:13 -0400

    added project settings and organized assets folder

---
+++ Assets/Script/HeaderBar.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HeaderBar : MonoBehaviour
+{
+    public GameObject RenamePanel;
+    public GameObject ColorPanel;
+    public GameObject BrushPanel;
+	void Start()
+	{
+        RenamePanel.SetActive(false);
+		ColorPanel.SetActive(false);
+        BrushPanel.SetActive(false);
+	}
+
+    void Update()
+    {
+        HideIfClickedOutside(RenamePanel);
+        HideIfClickedOutside(ColorPanel);
+        HideIfClickedOutside(BrushPanel);
+    }
+    
+    public void GoToHome() 
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OpenRenamePanel() 
+    {
+        if (RenamePanel != null)
+        {
+            bool isActive = RenamePanel.activeSelf;
+            RenamePanel.SetActive(!isActive);
+            ColorPanel.SetActive(false);
+            BrushPanel.SetActive(false);
+        }
+    }
+
+    public void OpenColorPanel()
+    {
+        if (ColorPanel != null)
+        {
+            bool isActive = ColorPanel.activeSelf;
+            ColorPanel.SetActive(!isActive);
+            RenamePanel.SetActive(false);
+            BrushPanel.SetActive(false);
+        }
+     }
+ 
+    public void OpenBrushPanel()
+    {
+        if (BrushPanel != null)
+        {
+            bool isActive = BrushPanel.activeSelf;
+            BrushPanel.SetActive(!isActive);
+            RenamePanel.SetActive(false);
+            ColorPanel.SetActive(false);
+        }
+     }
+
+    public void HideIfClickedOutside(GameObject panel) 
+    {
+        if (Input.GetMouseButton(0) && panel.activeSelf && 
+            !RectTransformUtility.RectangleContainsScreenPoint(
+                panel.GetComponent<RectTransform>(), 
+                Input.mousePosition, 
+                Camera.main)) {
+            panel.SetActive(false);
+        }
+    }
+}
---
+++ Assets/_Saving/MonoBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoBehaviourScript : MonoBehaviour {
+
+    // FIX ME: don't think i need now
+    // Start is called before the first frame update
+    // void Start() {
+
+    // }
+
+    // // Update is called once per frame
+    // void Update() {
+
+    // }
+
+    // bool isPaused = false;
+
+    // void Awake() {
+    //     Debug.Log("Awake");
+    // }
+
+    // void onEnable() {
+    //     Debug.Log("OnEnable");
+    // }
+
+    // void onApplicationPause(bool isApplicationPause) {
+    //     Debug.Log("isApplicationPause: ", isApplicationPause);
+    //     isPaused = isApplicationPause;
+    // }
+}
