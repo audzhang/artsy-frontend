480f0cb0d77f7039463aba45dc92dc677c787991
audzhang <audreyymz@gmail.com>
2020-10-20 21:51:24 -0400

    adding assets

---
+++ HomeScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeScreen : MonoBehaviour
+{
+    public void GoToShop ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void GoToDraw ()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
